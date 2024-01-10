376f584ba89ff7fd67b8a2166b3508be611a2f7d
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2024-01-10 16:38:12 +0100

    reformatting

--- Tests/Editor/VariousBaselines.cs
+++ Tests/Editor/VariousBaselines.cs
@@ -2,21 +2,15 @@
 // Refer to included LICENSE file for terms and conditions.
 
 using NUnit.Framework;
-using System;
 using Unity.PerformanceTesting;
-using UnityEngine;
-using Object = UnityEngine.Object;
 
 namespace CodeSmileEditor.Tests
 {
 	public class VariousBaselines
 	{
-		[Test] [Performance] public void Measure_EmptyCodeBlock() =>
+		[Test, Performance] public void Measure_EmptyCodeBlock() =>
 			Measure.Method(() => {}).DynamicMeasurementCount().Run();
 
-		[Test] [Performance] public void Measure_DebugLog_TestMessage() =>
-			Measure.Method(() => { Debug.Log("test"); }).DynamicMeasurementCount().Run();
-
-
+		//[Test] [Performance] public void Measure_DebugLog_TestMessage() => Measure.Method(() => { Debug.Log("test"); }).DynamicMeasurementCount().Run();
 	}
 }
